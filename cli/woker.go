b7dbb65937cc713fdc77af0428eea0984c4099c3
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2020-08-01 17:14:13 +0900

    add simple roundrobin

--- cli/woker.go
+++ cli/woker.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
+	"strings"
+	"sync/atomic"
 )
 
-type Nodes map[url.URL]bool
+// todo extended url.URL. struct
+type Nodes []url.URL
+
+type Algorithm = func(req *http.Request)
 
 type loadBalancer struct {
 	AllowScheme []string
 	Backends    Nodes
 	Transport   http.RoundTripper
-	Director    func(req *http.Request)
+	Algorithm   Algorithm
+	backends    uint32
+	req         uint32
 }
 
 //func (l *loadBalancer) IsAllowScheme(url *url.URL) bool {
@@ -89,7 +96,7 @@ func (l *loadBalancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	outreq := req.Clone(ctx)
-	l.Director(outreq)
+	l.Algorithm(outreq)
 	outreq.Close = false
 
 	// RFC 2616, section 13.5.1 https://tools.ietf.org/html/rfc2616#section-13.5.1
@@ -119,11 +126,15 @@ func (l *loadBalancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		res.Header.Del(v)
 	}
 
-	// copyHeader
-	for k, v := range res.Header {
-		for _, vv := range v {
-			w.Header().Add(k, vv)
+	copyHeader(w.Header(), res.Header)
+
+	announcedTrailers := len(res.Trailer)
+	if announcedTrailers > 0 {
+		trailerKeys := make([]string, 0, len(res.Trailer))
+		for k := range res.Trailer {
+			trailerKeys = append(trailerKeys, k)
 		}
+		w.Header().Add("Trailer", strings.Join(trailerKeys, ", "))
 	}
 	w.WriteHeader(res.StatusCode)
 
@@ -131,30 +142,93 @@ func (l *loadBalancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Println(err)
 	}
 
+	if len(res.Trailer) > 0 {
+		// Force chunking if we saw a response trailer.
+		// This prevents net/http from calculating the length for short
+		// bodies and adding a Content-Length.
+		if fl, ok := w.(http.Flusher); ok {
+			fl.Flush()
+		}
+	}
+
+	if len(res.Trailer) == announcedTrailers {
+		copyHeader(w.Header(), res.Trailer)
+		return
+	}
+	for k, vv := range res.Trailer {
+		k = http.TrailerPrefix + k
+		for _, v := range vv {
+			w.Header().Add(k, v)
+		}
+	}
 }
 
-func NewLoadBalancer() *loadBalancer {
-	u := url.URL{
-		Scheme: "http",
-		Host:   "localhost:3000",
+func copyHeader(dst http.Header, src http.Header) {
+	for k, v := range src {
+		for _, vv := range v {
+			dst.Add(k, vv)
+		}
 	}
+}
+
+func NewLoadBalancer(node Nodes) *loadBalancer {
+
+	backend := len(node)
+
+	//_ = httputil.NewSingleHostReverseProxy(&u).Director
 
 	return &loadBalancer{
 		AllowScheme: []string{"https", "http"},
-		Backends:    map[url.URL]bool{u: true},
-		// access golang reverse proxy Director
+		Backends:    node,
+		Transport:   nil,
+		backends:    uint32(backend),
+		req:         uint32(0),
+		// access golang reverse proxy Algorithm
 		// TODO fix
-		Director: httputil.NewSingleHostReverseProxy(&u).Director,
+
 	}
 }
 
 // todo
 // error handling
-// add original Director
+// add original Algorithm
 // run benchmark
 
 func main() {
-	l := NewLoadBalancer()
+	u := url.URL{
+		Scheme: "http",
+		Host:   "localhost:3000",
+	}
+
+	u2 := url.URL{
+		Scheme: "http",
+		Host:   "localhost:3030",
+	}
+	backends := []url.URL{u, u2}
+	l := NewLoadBalancer(backends)
+	l.Algorithm = func(req *http.Request) {
+		pick := l.Backends[l.req]
+
+		fmt.Println(l.req, uint32(len(l.Backends)-1))
+		if l.req >= uint32(len(l.Backends)-1) {
+			atomic.StoreUint32(&l.req, 0)
+		} else {
+			atomic.AddUint32(&l.req, 1)
+		}
+		fmt.Println(pick)
+		targetQuery := pick.RawQuery
+		req.URL.Scheme = pick.Scheme
+		req.URL.Host = pick.Host
+		req.URL.Path = pick.Path + req.URL.Path
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			req.Header.Set("User-Agent", "")
+		}
+	}
 	err := http.ListenAndServe(":8080", l)
 	if err != nil {
 		log.Fatalln(err)
